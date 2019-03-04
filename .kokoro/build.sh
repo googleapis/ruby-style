ce9dee72bf4434fcfe3df185a436233e80f22aed
TheRoyalTnetennba <paye@google.com>
2019-03-04 12:35:45 -0800

    setup for autorelease

---
+++ .kokoro/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This file runs tests for merges, PRs, and nightlies.
+# There are a few rules for what tests are run:
+#  * PRs run all non-acceptance tests for every library.
+#  * Merges run all non-acceptance tests for every library, and acceptance tests for all altered libraries.
+#  * Nightlies run all acceptance tests for every library.
+
+set -eo pipefail
+
+# Debug: show build environment
+env | grep KOKORO
+
+cd github/ruby-style/
+
+# Print out Ruby version
+ruby --version
+
+# Temporary workaround for a known bundler+docker issue:
+# https://github.com/bundler/bundler/issues/6154
+export BUNDLE_GEMFILE=
+
+# Capture failures
+EXIT_STATUS=0 # everything passed
+function set_failed_status {
+    EXIT_STATUS=1
+}
+
+git fetch --depth=10000
+python3 -m pip install git+https://github.com/googleapis/releasetool
+python3 -m releasetool publish-reporter-script > /tmp/publisher-script; source /tmp/publisher-script
+(bundle update && bundle exec rake kokoro:release) || set_failed_status
+
+exit $EXIT_STATUS
